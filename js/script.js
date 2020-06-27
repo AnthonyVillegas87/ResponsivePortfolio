f721f6eb928864e11532aabcb56ab966ca70ea0e
Anthony Villegas <villegasanthony87@yahoo.com>
2020-06-26 19:01:06 -0500

    added repository img to start linking projects to github. Added isotope plug-in for effects & animation. Modified JS for items to include animation options

--- js/script.js
+++ js/script.js
@@ -71,6 +71,18 @@ $(document).ready(function() {
 
     });
 
+    $("[data-fancybox]").fancybox();
+
+    $(".items").isotope({
+        filter: '*',
+        animationOptions: {
+            duration: 1500,
+            easing: 'linear',
+            queue: false
+
+        }
+    })
+
     
 
 });
